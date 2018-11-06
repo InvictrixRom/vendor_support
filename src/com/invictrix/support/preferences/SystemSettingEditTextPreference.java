9145b7e7d27f10ae9ed563a5493adef38189ac13
bigrushdog <randall.rushing@gmail.com>
2018-11-06 01:54:21 -0800

    Refactor for invictrix classpath

--- src/com/invictrix/support/preferences/SystemSettingEditTextPreference.java
+++ src/com/invictrix/support/preferences/SystemSettingEditTextPreference.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.dirtyunicorns.support.preferences;
+package com.invictrix.support.preferences;
 
 import android.content.Context;
 import android.support.v7.preference.EditTextPreference;
